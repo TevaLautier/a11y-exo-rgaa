cfa0c1aa50d46139bf51e4d52a4b1d149e402ccb
Teva Lautier <teva.lautier@soprasteria.com>
2026-08-13 16:10:25 +0200

    fix: icon on atomic-button

--- src/app/lib/ui/buttons/atomic-button.component.ts
+++ src/app/lib/ui/buttons/atomic-button.component.ts
@@ -1,13 +1,17 @@
-import { Component, input } from '@angular/core';
+import { Component, input, output } from '@angular/core';
+import { MatIcon } from "@angular/material/icon";
 
 @Component({
   selector: 'atomic-button',
   standalone: true,
   templateUrl: './atomic-button.component.html',
-  styleUrl: './atomic-button.component.scss'
+  styleUrl: './atomic-button.component.scss',
+  imports: [MatIcon]
 })
 export class AtomicButtonComponent {
   readonly type = input<'button' | 'submit'>('button');
-  readonly action = input<() => void>(() => {});
+  readonly action = output();
+  readonly icon = input<string | null>(null);
   readonly color = input<'primary' | 'secondary' | 'tertiary'>('primary');
+  readonly iconPosition = input<'left' | 'right'>('left');
 }
