ac46b9644baba9e5caa976c6371440d3c5baa3ff
Teva Lautier <teva.lautier@soprasteria.com>
2026-08-14 14:47:09 +0200

    fix: routerLink

--- src/app/lib/ui/buttons/atomic-button-icon.component.ts
+++ src/app/lib/ui/buttons/atomic-button-icon.component.ts
@@ -1,10 +1,11 @@
 import { Component, input } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'atomic-button-icon',
   standalone: true,
-  imports: [MatIconModule],
+  imports: [MatIconModule, RouterLink],
   templateUrl: './atomic-button-icon.component.html',
   styleUrl: './atomic-button-icon.component.scss'
 })
@@ -12,4 +13,5 @@ export class AtomicButtonIconComponent {
   readonly color = input<'primary' | 'secondary' | 'tertiary'>('primary');
   readonly icon = input.required<string>();
   readonly label = input<string>('Action');
+  readonly link = input<any>();
 }
