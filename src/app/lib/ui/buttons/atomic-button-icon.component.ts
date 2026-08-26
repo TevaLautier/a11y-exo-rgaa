aa7ac51957de43986ed224fc35b01bdcf2c7a28e
Teva Lautier <teva.lautier@soprasteria.com>
2026-08-26 17:41:45 +0200

    fix: event action

--- src/app/lib/ui/buttons/atomic-button-icon.component.ts
+++ src/app/lib/ui/buttons/atomic-button-icon.component.ts
@@ -1,17 +1,18 @@
-import { Component, input } from '@angular/core';
+import { Component, input, output } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
-import { RouterLink } from "@angular/router";
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'atomic-button-icon',
   standalone: true,
   imports: [MatIconModule, RouterLink],
   templateUrl: './atomic-button-icon.component.html',
-  styleUrl: './atomic-button-icon.component.scss'
+  styleUrl: './atomic-button-icon.component.scss',
 })
 export class AtomicButtonIconComponent {
   readonly color = input<'primary' | 'secondary' | 'tertiary'>('primary');
   readonly icon = input.required<string>();
   readonly label = input<string>('Action');
   readonly link = input<any>();
+  readonly action = output();
 }
